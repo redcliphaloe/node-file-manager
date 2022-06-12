e87df560af0d4e99ead9505b2cd4b0a10b1037ae
redcliphaloe <redcliphaloe@gmail.com>
2022-06-12 23:27:03 +0300

    feat: add full os info requirements

--- osi.js
+++ osi.js
@@ -8,8 +8,24 @@ export const getInfo = async (prm) => {
       case '--EOL':
         res = JSON.stringify(os.EOL.toString());
         break;
+      case '--cpus':
+        const cpus = os.cpus();
+        res = `${cpus.length}\n`;
+        for (let i =0; i < cpus.length; i++) {
+          res += `${cpus[i].model}\n`;
+        }
+        break;
+      case '--homedir':
+        res = os.homedir();
+        break;
+      case '--username':
+        res = os.userInfo().username;
+        break;
+      case '--architecture':
+        res = os.arch();
+        break;
       default:
-        process.stdout.write('Operation failed\n');
+        process.stdout.write('Invalid input\n');
         base.printCurrentDir();
         return;
         break;
