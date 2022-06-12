8c65cab05604451b4398893be99a22e67a0ce97b
redcliphaloe <redcliphaloe@gmail.com>
2022-06-12 22:01:18 +0300

    refactor: change hash with streams

--- hash.js
+++ hash.js
@@ -1,14 +1,24 @@
 import * as base from './base.js';
-import fs from 'fs/promises';
+import fs from 'fs';
 import crypto from 'crypto';
 
 export const getHash = async (path) => {
   try {
-    const text = await fs.readFile(path, 'utf8');
-    const hash = crypto.createHash('sha256');
-    hash.update(text);
-    process.stdout.write(`${hash.digest('hex')}\n`);
-    base.printCurrentDir();
+    const rs = fs.createReadStream(path, 'utf8');
+    let text = '';
+    rs.on('data', (chunk) => {
+      text += chunk;
+    });
+    rs.on('end', () => {
+      const hash = crypto.createHash('sha256');
+      hash.update(text);
+      process.stdout.write(`${hash.digest('hex')}\n`);
+      base.printCurrentDir();
+    });
+    rs.on('error', () => {
+      process.stdout.write('Operation failed\n');
+      base.printCurrentDir();
+    });
   } catch (err) {
     process.stdout.write('Operation failed\n');
     base.printCurrentDir();
