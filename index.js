a267552d4824281bc9d24e2d76666606d8395fba
redcliphaloe <redcliphaloe@gmail.com>
2022-06-12 20:25:51 +0300

    feat: add base requirements

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('iniit...');
+import os from 'os';
+
+export const getUsername = () => {
+  const dummy = 'stranger';
+  try {
+    return process.argv[2].replace(/--username=/i, '') || dummy;
+  } catch (error) {
+    return dummy;
+  }
+}
+
+export const enter = () => {
+  process.stdout.write(`Welcome to the File Manager, ${getUsername()}!\n`);
+  process.chdir(os.homedir());
+  printCurrentDir();  
+}
+
+export const exit = () => {
+  process.stdout.write(`\nThank you for using File Manager, ${getUsername()}!`);
+  process.exit();
+}
+
+export const printCurrentDir = () => {
+  process.stdout.write(`You are currently in ${process.cwd()}\n`);
+}
+
+export const printInvalid = () => {
+  process.stdout.write('Invalid input\n');
+}
+
+process.on('SIGINT', exit);
+
+process.stdin.on('data', (chunk) => {
+  switch (chunk.toString().trim()) {
+    case '.exit':
+      exit();
+      break;
+    default:
+      printInvalid();
+      break;
+  }
+  printCurrentDir();
+});
+
+enter();
