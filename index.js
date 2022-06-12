f1e35819263c05d86b8cec6d28318e5b503b7589
redcliphaloe <redcliphaloe@gmail.com>
2022-06-12 21:05:24 +0300

    refactor: move functionality to base & nwd modules

--- index.js
+++ index.js
@@ -1,79 +1,27 @@
-import os from 'os';
-import fs from 'fs/promises';
+import * as base from './base.js';
+import * as nwd from './nwd.js';
 
-export const getUsername = () => {
-  const dummy = 'stranger';
-  try {
-    return process.argv[2].replace(/--username=/i, '') || dummy;
-  } catch (error) {
-    return dummy;
-  }
-}
-
-export const enter = () => {
-  process.stdout.write(`Welcome to the File Manager, ${getUsername()}!\n`);
-  process.chdir(os.homedir());
-  printCurrentDir();  
-}
-
-export const exit = () => {
-  process.stdout.write(`\nThank you for using File Manager, ${getUsername()}!`);
-  process.exit();
-}
-
-export const printCurrentDir = () => {
-  process.stdout.write(`You are currently in ${process.cwd()}\n`);
-}
-
-export const printInvalid = () => {
-  process.stdout.write('Invalid input\n');
-  printCurrentDir();
-}
-
-export const up = () => {
-  process.chdir('..');
-  printCurrentDir();
-}
-
-export const cd = (path) => {
-  process.chdir(path);
-  printCurrentDir();
-}
-
-export const ls = async () => {
-  try {
-    const files = await fs.readdir(process.cwd());
-    for (const file of files) {
-      process.stdout.write(`${file}\n`);
-    }
-    printCurrentDir();
-  } catch (err) {
-    process.stdout.write(`${err}\n`);
-    printCurrentDir();
-  }
-}
-
-process.on('SIGINT', exit);
+process.on('SIGINT', base.exit);
 
 process.stdin.on('data', (chunk) => {
   const cmd = chunk.toString().trim().split(' ');  
   switch (cmd[0]) {
     case '.exit':
-      exit();
+      base.exit();
       break;
     case 'up':
-      up();
+      nwd.up();
       break;
     case 'cd':
-      cd(cmd[1]);
+      nwd.cd(cmd[1]);
       break;
     case 'ls':
-      ls();
+      nwd.ls();
       break;
     default:
-      printInvalid();
+      base.printInvalid();
       break;
   }
 });
 
-enter();
+base.enter();
